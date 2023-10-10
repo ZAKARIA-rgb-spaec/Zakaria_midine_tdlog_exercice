2c58451df45bf3be70baf53fb4d40fb80a4410ed
Zakaria midine <126424638+ZAKARIA-rgb-spaec@users.noreply.github.com>
2023-10-10 15:23:48 +0100

    Update exo2.py

--- exo2/exo2.py
+++ exo2/exo2.py
@@ -28,3 +28,34 @@ fixed_tests_False = (
     ( "spam",    "eggs"  )
 )
 """
+def solution(string, ending):
+    return string.endswith(ending)
+
+# Unit tests
+fixed_tests_True = (
+    ("samurai", "ai"),
+    ("ninja", "ja"),
+    ("sensei", "i"),
+    ("abc", "abc"),
+    ("abcabc", "bc"),
+    ("fails", "ails"),
+)
+
+fixed_tests_False = (
+    ("sumo", "omo"),
+    ("samurai", "ra"),
+    ("abc", "abcd"),
+    ("ails", "fails"),
+    ("this", "fails"),
+    ("spam", "eggs"),
+)
+
+for test_case in fixed_tests_True:
+    string, ending = test_case
+    result = solution(string, ending)
+    print(f"String: '{string}', Ending: '{ending}' -> Result: {result} (Expected: True)")
+
+for test_case in fixed_tests_False:
+    string, ending = test_case
+    result = solution(string, ending)
+    print(f"String: '{string}', Ending: '{ending}' -> Result: {result} (Expected: False)")
